e6772e358e4362f8cf5d7efb34902cff1637c7da
TinevimboMusingadi <tine2musi1@gmail.com>
2026-03-20 00:06:40 +0200

    Add setup.py for editable installation

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="omniembed",
+    version="0.1.0",
+    description="Reverse-Engineering Universal Embeddings from Qwen3-Omni",
+    author="Tinevimbo Musingadi",
+    packages=find_packages(),
+    install_requires=[
+        # Dependencies are managed in requirements.txt
+    ],
+    python_requires=">=3.10",
+)
